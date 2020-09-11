9a77125cd8774b84b8e83b0966577cf2fa7a3c50
oTienTienTyo <otientientyo@gmail.com>
2020-09-11 19:31:49 +0700

    Testing
    test exapmle session 2

---
+++ example session 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (void)
+{	int a;
+	scanf("%d", &a);
+	int C;
+	C=a*6;
+	printf("%d",C);
+	return 0;
+}
---
+++ test 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+{ char abc;
+int xyz;
+float length;
+double area;
+long liteyrs;
+short arm;
+}
